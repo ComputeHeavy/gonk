bc8d656e4bb72078bb5973238d63e979d62d727f
TACIXAT <982598+TACIXAT@users.noreply.github.com>
2023-09-08 18:40:50 -0700

    Finish sqlite validator. Untested.

--- sqlite.py
+++ sqlite.py
@@ -162,17 +162,19 @@ class State:
         if core.is_schema(event.object.name):
             cur.execute(
                 "INSERT INTO schemas (name, uuid, version) VALUES (?, ?, ?)",
-                (event.object.name, event.object.uuid, event.object.version))   
+                (event.object.name, 
+                    str(event.object.uuid), 
+                    event.object.version))   
 
         cur.execute("""INSERT INTO object_event_link 
             (object_uuid, object_version, event_uuid) 
             VALUES (?, ?, ?)""", 
-            (event.object.uuid, event.object.version, event.uuid))
+            (str(event.object.uuid), event.object.version, event.uuid))
         
         cur.execute("""INSERT INTO object_status 
             (object_uuid, object_version, status) 
             VALUES (?, ?, ?)""", 
-            (event.object.uuid, 
+            (str(event.object.uuid), 
                 event.object.version, 
                 StatusT.CREATE_PENDING.name))
 
@@ -181,17 +183,19 @@ class State:
         if core.is_schema(event.object.name):
             cur.execute(
                 "INSERT INTO schemas (name, uuid, version) VALUES (?, ?, ?)",
-                (event.object.name, event.object.uuid, event.object.version))   
+                (event.object.name, 
+                    str(event.object.uuid), 
+                    event.object.version))   
 
         cur.execute("""INSERT INTO object_event_link 
             (object_uuid, object_version, event_uuid) 
             VALUES (?, ?, ?)""", 
-            (event.object.uuid, event.object.version, event.uuid))
+            (str(event.object.uuid), event.object.version, event.uuid))
         
         cur.execute("""INSERT INTO object_status 
             (object_uuid, object_version, status) 
             VALUES (?, ?, ?)""", 
-            (event.object.uuid, 
+            (str(event.object.uuid), 
                 event.object.version, 
                 StatusT.CREATE_PENDING.name))
 
@@ -224,12 +228,12 @@ class State:
         cur.execute("""INSERT INTO annotation_event_link 
             (annotation_uuid, annotation_version, event_uuid) 
             VALUES (?, ?, ?)""", 
-            (event.annotation.uuid, event.annotation.version, event.uuid))
+            (str(event.annotation.uuid), event.annotation.version, event.uuid))
 
         cur.execute("""INSERT INTO annotation_status 
             (annotation_uuid, annotation_version, status) 
             VALUES (?, ?, ?)""", 
-            (event.annotation.uuid, 
+            (str(event.annotation.uuid), 
                 event.annotation.version, 
                 StatusT.CREATE_PENDING.name))
 
@@ -238,12 +242,12 @@ class State:
         cur.execute("""INSERT INTO annotation_event_link 
             (annotation_uuid, annotation_version, event_uuid) 
             VALUES (?, ?, ?)""", 
-            (event.annotation.uuid, event.annotation.version, event.uuid))
+            (str(event.annotation.uuid), event.annotation.version, event.uuid))
 
         cur.execute("""INSERT INTO annotation_status 
             (annotation_uuid, annotation_version, status) 
             VALUES (?, ?, ?)""", 
-            (event.annotation.uuid, 
+            (str(event.annotation.uuid), 
                 event.annotation.version, 
                 StatusT.CREATE_PENDING.name))
 
@@ -396,7 +400,7 @@ class State:
                 cur.execute("""INSERT INTO object_status 
                     (object_uuid, object_version, status) 
                     VALUES (?, ?, ?)""", 
-                    (event.object.uuid, 
+                    (str(event.object.uuid), 
                         event.object.version, 
                         StatusT.CREATE_REJECTED.name))
             elif isinstance(target, ObjectDeleteEvent):
@@ -434,7 +438,7 @@ class State:
                 cur.execute("""INSERT INTO annotation_status 
                     (annotation_uuid, annotation_version, status) 
                     VALUES (?, ?, ?)""", 
-                    (event.annotation.uuid, 
+                    (str(event.annotation.uuid), 
                         event.annotation.version, 
                         StatusT.CREATE_REJECTED.name))
             elif isinstance(target, ObjectDeleteEvent):
@@ -468,73 +472,125 @@ class State:
         cur.execute("""DELETE FROM owners WHERE public_key = ?""",
             (event.public_key.hex()))
 
-    ## HERE
+    ## TODO: validate no replay
 
     def _validate_object_create(self, event: ObjectCreateEvent):
-        if is_schema(event.object.name):
-            if self.state.schema_exists(name=event.object.name):
+        cur = self.con.cursor()
+        if core.is_schema(event.object.name):
+            cur.execute('''SELECT COUNT(*) FROM schemas WHERE name = ?''',
+                (event.object.name,))
+            count, = cur.fetchone()
+            if count != 0:
                 raise ValidationError("schema name already in use")
 
-        if self.state.object_exists(uuid_=event.object.uuid):
-            raise ValidationError("UUID already exists in object store")
+        cur.execute('''SELECT COUNT(*) 
+            FROM events 
+            WHERE event->>'$.object.uuid' = ?''',
+            (str(event.object.uuid),))
+
+        count, = cur.fetchone()
+        if count != 0:
+            raise ValidationError("object with UUID already exists")
 
         if event.object.version != 0:
             raise ValidationError(
                 "object version must be zero in create event")
 
     def _validate_object_update(self, event: ObjectCreateEvent):
-        if not self.state.object_exists(uuid_=event.object.uuid):
-            raise ValidationError("UUID not found in object store")
+        cur = self.con.cursor()
+        cur.execute('''SELECT event 
+            FROM events 
+            WHERE event->>'$.object.uuid' = ?''', 
+            (str(event.object.uuid),))
+        versions_json = cur.fetchall()
+        if len(versions_json) == 0:
+            raise ValidationError("no objects with UUID found")
 
-        if is_schema(event.object.name):
-            if not self.state.schema_exists(name=event.object.name):
-                raise ValidationError("schema name does not exist")
-
-        versions = self.state.object_versions(uuid_=event.object.uuid)
-        if versions[-1].uuid != event.object.uuid:
-            raise ValidationError("unexpected UUID for schema name")
+        versions_data = [json.loads(ea) for ea, in versions_json]
+        
+        versions = [getattr(core, ea["type"]).deserialize(ea).object 
+            for ea in versions_data]
 
         if is_schema(versions[-1].name):
             if versions[-1].name != event.object.name:
                 raise ValidationError("schema names may not change")
+        else:
+            if is_schema(event.object.name):
+                raise ValidationError("object may not become schema")
 
         if event.object.version != len(versions):
             raise ValidationError(
                 f"object version should be {len(versions)}")
 
     def _validate_object_delete(self, event: ObjectDeleteEvent):
-        if self.state.schema_exists(uuid_=event.object_identifier.uuid):
+        cur = self.con.cursor()
+        cur.execute('''SELECT COUNT(*) FROM schemas WHERE uuid = ?''',
+            (str(event.object_identifier.uuid),))
+        
+        count, = cur.fetchone()
+        if count != 0:
             raise ValidationError("schemas can not be deleted")
 
-        identifier = event.object_identifier
-        if not self.state.object_exists(identifier=identifier):
+        cur.execute('''SELECT COUNT(*)
+            FROM events 
+            WHERE event->>'$.object.uuid' = ?
+                AND event->>'$.object.version' = ?''', 
+            (str(event.object_identifier.uuid), 
+                event.object_identifier.version))
+
+        count, = cur.fetchone()
+        if count == 0:
             raise ValidationError("object identifier not found")
 
-        status = self.state.object_status(identifier=identifier)
-        if StatusT.CREATE_REJECTED in status:
+        cur.execute('''SELECT status
+            FROM object_status 
+            WHERE object_uuid = ? 
+                AND object_version = ?''',
+            (str(event.object_identifier.uuid), 
+                event.object_identifier.version))
+
+        status = set([getattr(core.StatusT, ea) for ea, in cur.fetchall()])
+        if core.StatusT.CREATE_REJECTED in status:
             raise ValidationError("cannot delete a rejected object")
 
-        if StatusT.DELETE_PENDING in status:
+        if core.StatusT.DELETE_PENDING in status:
             raise ValidationError("object version pending deletion")
 
-        if StatusT.DELETE_ACCEPTED in status:
+        if core.StatusT.DELETE_ACCEPTED in status:
             raise ValidationError("object version already deleted")
 
     def _validate_annotation_create(self, event: AnnotationCreateEvent):
-        if self.state.annotation_exists(uuid_=event.annotation.uuid):
-            raise ValidationError(
-                "UUID already exists in annotation store")
+        cur = self.con.cursor()
+        cur.execute('''SELECT COUNT(*) 
+            FROM events 
+            WHERE event->>'$.annotation.uuid' = ?''',
+            (str(event.annotation.uuid),))
+
+        count, = cur.fetchone()
+        if count != 0:
+            raise ValidationError("annotation with UUID already exists")
 
         if event.annotation.version != 0:
             raise ValidationError(
                 "annotation version must be zero in create event")
 
         for identifier in event.object_identifiers:
-            if not self.state.object_exists(identifier=identifier):
-                raise ValidationError(
-                    "object identifier not found in object store")
+            cur.execute('''SELECT COUNT(*)
+                FROM events 
+                WHERE event->>'$.object.uuid' = ?
+                    AND event->>'$.object.version' = ?''', 
+                (str(identifier.uuid), identifier.version))
+            count, = cur.fetchone()
+            if count == 0:
+                raise ValidationError("object identifier not found")
 
-            status = self.state.object_status(identifier=identifier)
+            cur.execute('''SELECT status
+                FROM object_status 
+                WHERE object_uuid = ? 
+                    AND object_version = ?''',
+                (str(identifier.uuid), identifier.version))
+
+            status = set([getattr(core.StatusT, ea) for ea, in cur.fetchall()])
             if StatusT.CREATE_REJECTED in status:
                 raise ValidationError(
                     "rejected objects cannot be annotated")
@@ -543,23 +599,48 @@ class State:
                 raise ValidationError(
                     "deleted objects cannot be annotated")
 
-            if self.state.schema_exists(identifier=identifier):
-                raise ValidationError("schemas can not be annotated")
+            cur.execute('''SELECT COUNT(*) FROM schemas WHERE uuid = ?''',
+                (str(identifier.uuid),))
+            count, = cur.fetchone()
+            if count != 0:
+                raise ValidationError("schemas can not be deleted")
 
     def _validate_annotation_update(self, event: AnnotationUpdateEvent):
-        if not self.state.annotation_exists(uuid_=event.annotation.uuid):
-            raise ValidationError("UUID not found in annotation store")
+        cur = self.con.cursor()
+        cur.execute('''SELECT event 
+            FROM events 
+            WHERE event->>'$.annotation.uuid' = ?''', 
+            (str(event.annotation.uuid),))
 
-        versions = self.state.annotation_versions(uuid_=event.annotation.uuid)
-        if event.annotation.version != len(versions):
-            return f"Annotation version should be {len(versions)}."
+        versions_json = cur.fetchall()
+        if len(versions_json) == 0:
+            raise ValidationError("no annotations with UUID found")
+
+        if event.annotation.version != len(versions_json):
+            raise ValidationError(
+                f"annotation version should be {len(versions_json)}.")
 
     def _validate_annotation_delete(self, event: AnnotationDeleteEvent):
-        identifier = event.annotation_identifier
-        if not self.state.annotation_exists(identifier=identifier):
+        cur = self.con.cursor()
+        cur.execute('''SELECT COUNT(*)
+            FROM events 
+            WHERE event->>'$.annotation.uuid' = ?
+                AND event->>'$.annotation.version' = ?''', 
+            (str(event.annotation_identifier.uuid), 
+                event.annotation_identifier.version))
+
+        count, = cur.fetchone()
+        if count == 0:
             raise ValidationError("annotation identifier not found")
 
-        status = self.state.annotation_status(identifier=identifier)
+        cur.execute('''SELECT status
+            FROM annotation_status 
+            WHERE annotation_uuid = ? 
+                AND annotation_version = ?''',
+            (str(event.annotation_identifier.uuid), 
+                event.annotation_identifier.version))
+
+        status = set([getattr(core.StatusT, ea) for ea, in cur.fetchall()])
         if StatusT.CREATE_REJECTED in status:
             raise ValidationError("cannot delete a rejected annotation")
 
@@ -569,26 +650,61 @@ class State:
         if StatusT.DELETE_ACCEPTED in status:
             raise ValidationError("annotation already deleted")
 
-    def _validate_review_accept(self, event: ReviewAcceptEvent):
-        if not self.state.event_pending(uuid_=event.event_uuid):
-            raise ValidationError("target event not pending")
+    def _validate_review(self, event: ReviewAcceptEvent):
+        cur = self.con.cursor()
+        cur.execute('''SELECT COUNT(*)
+            FROM event_review_link 
+            WHERE event_uuid = ?''', 
+            (str(event.event_uuid),))
 
-        if not self.state.owner_exists(public_key=event.signer):
+        count, = cur.fetchone()
+        if count != 0:
+            raise ValidationError("event already reviewed")
+
+        cur.execute('''SELECT event 
+            FROM events 
+            WHERE event->>'$.uuid' = ?''', 
+            (str(event.event_uuid),))
+
+        res = cur.fetchone()
+        if res is None:
+            raise ValidationError("no events with event UUID found")
+
+        target_json, = res
+        target_data = json.loads(target_json)
+        target_event = getattr(
+            core, target_data["type"]).deserialize(target_data)
+
+        if not isinstance(target_event, (AnnotationEvent, ObjectEvent)):
+            raise ValidationError("review on non object or annotation event")
+
+        cur.execute('''SELECT COUNT(*) 
+            FROM owners 
+            WHERE public_key = ?''',
+            (event.signer.hex(),))
+        count, = cur.fetchone()
+        if count == 0:
             raise ValidationError("review event from non-owner")
+
+    def _validate_review_accept(self, event: ReviewAcceptEvent):
+        self._validate_review(event)
 
     def _validate_review_reject(self, event: ReviewRejectEvent):
-        if not self.state.event_pending(uuid_=event.event_uuid):
-            raise ValidationError("target event not pending")
-
-        if not self.state.owner_exists(public_key=event.signer):
-            raise ValidationError("review event from non-owner")
-
+        self._validate_review(event)
+        
     def _validate_owner_add(self, event: OwnerAddEvent):
-        if self.state.owner_exists(public_key=event.public_key):
-            raise ValidationError("owner already present")
+        cur = self.con.cursor()
+        cur.execute('''SELECT public_key FROM owners''')
 
-        if len(self.state.owners()) > 0:
-            if not self.state.owner_exists(public_key=event.signer):
+        owners = {public_key for public_key, in cur.fetchall()}
+        if len(owners) == 0:
+            raise ValidationError("dataset has no owners to remove")
+
+        if len(owners) > 0:
+            if event.public_key in owners:
+                raise ValidationError("owner already present")
+
+            if event.signer not in owners:
                 raise ValidationError("only owners can add owners")
         else:
             if event.public_key != event.signer:
@@ -596,21 +712,27 @@ class State:
                     "first owner add event must be self signed")
 
     def _validate_owner_remove(self, event: OwnerRemoveEvent):
-        if not self.state.owner_exists(public_key=event.public_key):
-            raise ValidationError("owner not present")
+        cur = self.con.cursor()
+        cur.execute('''SELECT id, public_key FROM owners''')
 
-        if not self.state.owner_exists(public_key=event.signer):
-            raise ValidationError("only owners can remove owners")
+        owners = cur.fetchall()
+        if len(owners) == 0:
+            raise ValidationError("dataset has no owners to remove")
 
-        owners = self.state.owners()
-        if len(owners) == 1:
+        ranks = {bytes.fromhex(public_key): id_ for id_, public_key in owners}
+
+        if event.signer not in ranks:
+            raise ValidationError("only owners may remove owners")
+
+        if event.public_key not in ranks:
+            raise ValidationError("target key is not an owner")
+        
+        if len(ranks) == 1:
             raise ValidationError(
                 "removing owner would leave the dataset ownerless")
 
-        target_rank = owners.index(event.public_key)
-        actor_rank = owners.index(event.signer)
+        target_rank = ranks[event.public_key]
+        signer_rank = ranks[event.signer]
 
-        if actor_rank > target_rank:
+        if signer_rank > target_rank:
             raise ValidationError("cannot remove a higher ranking owner")
-
-    
